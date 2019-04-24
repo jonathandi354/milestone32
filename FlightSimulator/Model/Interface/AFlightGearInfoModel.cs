3a0fb8ee4beb5123be284bda969be664b7bd6fa4
Naama <naamaberman@gmail.com>
2019-04-24 17:52:32 +0300

    Added Naama's files

---
+++ FlightSimulator/Model/Interface/AFlightGearInfoModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using FlightSimulator.ViewModels;
+
+namespace FlightSimulator.Model.Interface
+{
+    public abstract class AFlightGearInfoModel : BaseNotify
+    {
+
+        private readonly object syncLock = new object();
+
+        private double lon;
+        private double lat;
+
+        public double Lon
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    return lon;
+                }
+            }
+            set
+            {
+                lock (syncLock)
+                {
+                    lon = value;
+                    NotifyPropertyChanged("Lon");
+                }
+
+            }
+        }
+        public double Lat
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    return lat;
+                }
+            }
+            set
+            {
+                lock (syncLock)
+                {
+                    lat = value;
+                    NotifyPropertyChanged("Lat");
+                }
+
+            }
+        }        
+
+        public abstract void Connect(IPAddress ip, int port);
+
+    }
+}
--- FlightSimulator/ViewModels/FlightBoardViewModel.cs
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -1,7 +1,10 @@
-﻿using FlightSimulator.Model.Interface;
+﻿using FlightSimulator.Model;
+using FlightSimulator.Model.Interface;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +13,65 @@ namespace FlightSimulator.ViewModels
     public class FlightBoardViewModel : BaseNotify
     {
 
+        private double lon;
+        private double lat;
+
         public double Lon
         {
-            get;
-        }
+            get
+            {
+                return lon;
+            }
+            set
+            {
+                lon = value;
+                NotifyPropertyChanged("Lon");
 
+            }
+        }
         public double Lat
         {
-            get;
+            get { return lat; }
+            set
+            {
+                lat = value;
+                NotifyPropertyChanged("Lat");
+            }
+        }
+        private AFlightGearInfoModel model;
+
+        public FlightBoardViewModel(AFlightGearInfoModel m)
+        {
+            this.model = m;
+            model.PropertyChanged += EventHandler;
         }
+
+
+        private void EventHandler(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("Lon"))
+            {
+                if (this.model.Lon != this.Lon)
+                {
+                    this.Lon = this.model.Lon;
+
+                }
+
+            }
+            if (e.PropertyName.Equals("Lat"))
+            {
+                if (this.model.Lat != this.Lat)
+                {
+                    this.Lat = this.model.Lat;
+
+                }
+            }
+        } 
+
+        public void ConnectModel(IPAddress ip, int port)
+        {
+            this.model.Connect(ip, port);
+        }
+
     }
 }
